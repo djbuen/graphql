22e522a77cb86e982a91e5d1f34b51cdbb745222
dave <daveb@ekconsulting.net>
2019-12-19 10:55:37 +0800

    initial commit

---
+++ args.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { graphql, buildSchema } = require('graphql');
+
+
+var schema = buildSchema(`
+  type Query {
+      rollDice(numDice: Int!, numSides: Int): [Int]
+        }
+`);
+
+var root = {
+  rollDice: ({numDice, numSides}) => {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  }
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
